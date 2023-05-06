6d81d119fecedd95e5921ced855b577e1c348750
Steven Jiang <stevenjiangnz@gmail.com>
2023-05-06 07:30:10 +0000

    add container

--- Screen.Deploy/terraform/main.tf
+++ Screen.Deploy/terraform/main.tf
@@ -5,7 +5,6 @@ resource "azurerm_resource_group" "screen_rg" {
   tags = var.tags
 }
 
-
 resource "azurerm_storage_account" "screen_sa" {
   name                     = "sascreen4sj"
   resource_group_name      = azurerm_resource_group.screen_rg.name
@@ -14,4 +13,11 @@ resource "azurerm_storage_account" "screen_sa" {
   account_replication_type = "LRS"
 
   tags = var.tags
+}
+
+
+resource "azurerm_storage_container" "screen_container" {
+  name                  = "screencontainer"
+  storage_account_name  = azurerm_storage_account.screen_sa.name
+  container_access_type = "private"
 }
