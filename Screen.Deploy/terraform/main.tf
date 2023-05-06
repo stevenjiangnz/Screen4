367c2218e6b9a901a0935ffb737121e860a3f363
Steven Jiang <stevenjiangnz@gmail.com>
2023-05-06 06:48:58 +0000

    add tags

--- Screen.Deploy/terraform/main.tf
+++ Screen.Deploy/terraform/main.tf
@@ -1,4 +1,9 @@
 resource "azurerm_resource_group" "example" {
   name     = "WG-Screen4"
   location = "australiaeast"
+
+  tags = {
+    environment = "dev"
+    owner       = "Steven Jiang"
+  }
 }
