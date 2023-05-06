fdc8ebb3b95a2027a42427075b12a8e00bf14d55
Steven Jiang <stevenjiangnz@gmail.com>
2023-05-06 06:59:55 +0000

    add storage account

--- Screen.Deploy/terraform/main.tf
+++ Screen.Deploy/terraform/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "screen_rg" {
   name     = "WG-Screen4"
   location = "australiaeast"
 
@@ -7,3 +7,16 @@ resource "azurerm_resource_group" "example" {
     owner       = "Steven Jiang"
   }
 }
+
+
+resource "azurerm_storage_account" "screen_sa" {
+  name                     = "sascreen4sj"
+  resource_group_name      = azurerm_resource_group.screen_rg.name
+  location                 = "australiaeast"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+    owner       = "john.doe"
+  }
